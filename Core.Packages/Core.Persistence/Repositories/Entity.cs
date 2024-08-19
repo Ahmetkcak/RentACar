b65acae1b05df47e32ea28b11391d70e9c85d09b
AHMET KOCAK <f201213038@ktun.edu.tr>
2024-08-19 14:41:26 +0300

    feat(Core.Packages): implemented Advanced Entity Framework

--- Core.Packages/Core.Persistence/Repositories/Entity.cs
+++ Core.Packages/Core.Persistence/Repositories/Entity.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Core.Persistence.Repositories;
 
-public class Entity<TId>
+public class Entity<TId> : IEntityTimestamps
 {
     public TId Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
-    public DateTime? DeletedAt { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
 
     public Entity()
     {
